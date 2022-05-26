49c72bd2a4280eb3d359fdbe6aa63cb0fd5ddaa6
Alma <alma.blitric@gmail.com>
2022-05-26 15:47:13 -0500

    Completed all

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -37,7 +37,7 @@ public class SearchController {
         } else {
     jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
-        model.addAttribute("jobs", jobs);
+        model.addAttribute("searchJobs", jobs);
         model.addAttribute("columns", columnChoices);
         return "search";
     }
